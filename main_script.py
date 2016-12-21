b2597ebc98aead757845e7241a74aa465b46c8cf
mehul sangekar <mehulnswork@gmail.com>
2016-12-21 18:31:35 +0900

    Version 1.0
    Update to include libraries for block statistics
    Separate library for SVM

--- main_script.py
+++ main_script.py
@@ -6,20 +6,19 @@ Predicting the split image properties for Cruse estimation
 
 """
 
+ #%%
 
 
-def predict_crust_images(para_list, svc_class):
-    predict_list =  svc_class.predict(para_list)        
-    return predict_list
-    
-def train_crust_images(para_list, labels_list, svc_class):
-    svc_class.fit(para_list, labels_list) 
-    return None
 
-def create_svm():
-    from sklearn import svm
-    svc_class = svm.SVC(kernel='poly')        
-    return svc_class
+ #%%
+
+def get_image_xyz_filenames(dir_splits, image_split_list, xyz_split_list, index):
+    path_image = dir_splits + '/' + image_split_list[index]
+    search_str =  image_split_list[index].split('.')[0]
+    print search_str
+    d = [ s for s in xyz_split_list if search_str in s]  
+    path_xyz = dir_splits + '/' + d[0]
+    return(path_image, path_xyz)
 
 def get_split_imagelist(dir_splits):
     import os
@@ -28,12 +27,64 @@ def get_split_imagelist(dir_splits):
     print('Number of JPEG files found:' + str(len(split_list)))
     return split_list    
 
-def get_image_stats(dir_splits, split_list):
-    path_image = dir_splits + '/' + split_list[0]
-    cv2_img, img_height, img_width =  load_opencv_image(path_image)
-    avg_r, avg_g, avg_b = get_avg_rgb(cv2_img)
-    avg_h, avg_s, avg_v = get_avg_hsv(cv2_img)
+def get_split_xyzlist(dir_splits):
+    import os
+    d = os.listdir(dir_splits)
+    split_list = [ s for s in d if '.txt' in s]
+    print('Number of txt files found:' + str(len(split_list)))
+    return split_list    
+
+ #%%
+def get_block_stats(dir_splits, image_split_list, xyz_split_list, path_blockstats):
+
+    file_blockstats = open(path_blockstats,'w')
+    
+    for index in range(10):
+        path_image, path_xyz = get_image_xyz_filenames(dir_splits, image_split_list, xyz_split_list, index)
+
+        
+        cv2_img, img_height, img_width =  load_opencv_image(path_image)
+        avg_r, avg_g, avg_b = get_avg_rgb(cv2_img)
+        avg_h, avg_s, avg_v = get_avg_hsv(cv2_img)
+        points_x, points_y, points_z   =  load_xyz(path_xyz)   
+        mean_z, std_z = get_xyz_moments(points_x, points_y, points_z)
+
+        file_blockstats.write(path_image + ',')
+        file_blockstats.write(str(avg_r) + ',')
+        file_blockstats.write(str(avg_g) + ',')
+        file_blockstats.write(str(avg_b))
+        file_blockstats.write('\n')    
+
+    file_blockstats.close()    
     return None
+
+ #%%
+
+def load_xyz(path_xyz):
+    import numpy as np
+    points_x = np.array([])
+    points_y = np.array([])
+    points_z = np.array([])
+    
+    file_xyz = open(path_xyz,'r')    
+    
+    for line in file_xyz.readlines():
+        d = line.split()
+        if(float(d[2]) > 0):
+            points_x = np.append(points_x,float(d[0]))
+            points_y = np.append(points_y,float(d[1]))
+            points_z = np.append(points_z,float(d[2]))
+    
+    file_xyz.close()        
+    return(points_x, points_y, points_z)
+
+def get_xyz_moments(points_x, points_y, points_z):    
+    import numpy as np
+    mean_z = np.mean(points_z)
+    std_z  = np.std(points_z)
+    return(mean_z, std_z)
+
+ #%%
 
 def load_opencv_image(path_image):
     import cv2
@@ -60,37 +111,35 @@ def get_avg_hsv(image):
     avg_s = avg_hsv[1]
     avg_v = avg_hsv[2]    
     return(avg_h, avg_s, avg_v)       
+   
 
 #%% Reading blocks from folder
 
+if __name__ == '__main__':
+    try:        
+        dir_splits = '/home/oplab/cruiseData/NT15-03/HPD1780/Publish/Splits'
+        path_blockstats = '/home/oplab/cruiseData/NT15-03/HPD1780/Publish/block_stats.txt'
+        
+        image_split_list = get_split_imagelist(dir_splits)
+        xyz_split_list   = get_split_xyzlist(dir_splits)
+        get_block_stats(dir_splits, image_split_list, xyz_split_list, path_blockstats)
+        
+        import crust_svm as csvm
+        
+        para_train_list, labels_train_list, para_predict_list, labels_verify_list = csvm.create_iris_dataset()
+        svc = csvm.create_svm()
+        print('Correct labels:' + str(labels_verify_list))
+        
+        csvm.train_crust_images(para_train_list, labels_train_list, svc)
+        labels_predict_list = csvm.predict_crust_images(para_predict_list, svc)
+        
+        print('SCM labels:' + str(labels_predict_list))
 
-dir_splits = '/home/oplab/cruiseData/NT15-03/HPD1780/Publish/Splits'
-split_list = get_split_imagelist(dir_splits)
-get_image_stats(dir_splits, split_list)
-
-import numpy as np
-from sklearn import datasets
-
-
-
-iris = datasets.load_iris()
-iris_X = iris.data
-iris_Y = iris.target
-np.random.seed(0)
-indices = np.random.permutation(len(iris_X))
-para_train_list   = iris_X[indices[:-10]] #all except last 10
-labels_train_list = iris_Y[indices[:-10]]
-para_predict_list  = iris_X[indices[-10:]] # only last 10
-labels_verify_list  = iris_Y[indices[-10:]]
-
-print('Correct labels:' + str(labels_verify_list))
-
-svc = create_svm()
-train_crust_images(para_train_list, labels_train_list, svc)
-labels_predict_list = predict_crust_images(para_predict_list, svc)
-
-print('SCM labels:' + str(labels_predict_list))
-print('Program exit')
+    except BaseException as e:
+        print(e)
+        raise
+    finally:
+        print('Execution Completed')
 
 #%% Extracting parameters from each block for analysis
 
